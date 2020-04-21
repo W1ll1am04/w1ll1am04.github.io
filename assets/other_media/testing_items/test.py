f043d4ba5bf9d7382153b8fcc0b6bd75544b55f2
William <43065399+W1ll1am04@users.noreply.github.com>
2020-04-21 19:38:21 +0200

    Add a file to test downloading and run instantly.
    Add a file to test downloading and run instantly from a script.

---
+++ assets/other_media/testing_items/test.py
@@ -0,0 +1,20 @@
+# This is a testing file.
+# Im using it to test if it will download properly and so and so on.
+
+# Define a string for "Other Welcome!"
+ow = "Other Welcome!"
+
+# Define a string for "Variable"
+var = "Variable"
+
+# Try something useless.
+try:
+  print(var)
+except:
+  print(ow)
+
+# Define a string for "Hello World!"
+hw = "Hello World!"
+
+# Print "Hello World!"
+print(hw)
